857a174c59acd848d810722872e5089c6e87232a
riazsarker <riaz.cse260@gmail.com>
2023-08-29 12:22:40 +0600

    events module added

--- index.js
+++
@@ -1,3 +0,0 @@
-const _ = require('lodash');
-
-console.log(_.last([1,2,3]));
---
+++ people.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+const people = ['sakib', 'masrafi', 'rafi'];
+const a = 6;
+
+function test() {
+    console.log('test');
+}
+module.exports = {
+    people,
+    a,
+    test,
+};
